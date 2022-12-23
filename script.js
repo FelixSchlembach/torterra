b0ce176c24be939de086556211b880918b87987a
Felix Schlembach <felix.schlembach@gmail.com>
2022-12-23 10:08:17 +0100

    design und funktion redo

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 /*-- script.js --*/
 
-function clickPhysikSubMenu(){
-    if(document.getElementById("sub_nav_bar_physik").style.display == "none"){
-        document.getElementById("sub_nav_bar_mathe").style.display = "none";
-        document.getElementById("sub_nav_bar_physik").style.display = "block";
-    }else{
-        document.getElementById("sub_nav_bar_physik").style.display = "none";
+var topics_dictionary = {"close_all": -1 ,"mathe": 0, "physik": 1};
+
+
+function clickDropDownButton(source=""){
+    for(var i=0; i<2; i++){
+        if(topics_dictionary[source] != i || topics_dictionary[source] == -1 || document.getElementsByClassName("dropdown")[topics_dictionary[source]].style.display == "block"){
+            document.getElementsByClassName("dropdown")[i].style.display = "none";
+            document.getElementsByClassName("topic_button")[i].style.backgroundColor = "transparent";
+            
+        }else{
+            document.getElementsByClassName("dropdown")[i].style.display = "block";
+            document.getElementsByClassName("topic_button")[i].style.backgroundColor = "#25252B";
+        }
     }
 }
-function clickMatheSubMenu(){
-    if(document.getElementById("sub_nav_bar_mathe").style.display == "none"){
-        document.getElementById("sub_nav_bar_physik").style.display = "none";
-        document.getElementById("sub_nav_bar_mathe").style.display = "inline";
+function clickContentDropDownButton(source=""){
+    if(document.getElementsByClassName("dropdown")[topics_dictionary[source]+2].style.display == "block"){
+        document.getElementsByClassName("dropdown")[topics_dictionary[source]+2].style.display = "none";
+        
     }else{
-        document.getElementById("sub_nav_bar_mathe").style.display = "none";
+        document.getElementsByClassName("dropdown")[topics_dictionary[source]+2].style.display = "block";
     }
 }
-function clickRestOfPage(){
-    document.getElementById("sub_nav_bar_physik").style.display = "none";
-    document.getElementById("sub_nav_bar_mathe").style.display = "none";
+
+
+function getTopicContent(source="", destination=""){
+    document.getElementById(destination).innerHTML = document.getElementsByClassName("dropdown_content")[topics_dictionary[source.toLowerCase()]].innerHTML;
 }
-  
